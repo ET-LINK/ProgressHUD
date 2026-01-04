32ba0cb61314439992bb8343814f63397d1cd48b
LK-LINK <ke.liful@gmail.com>
2026-01-04 11:04:11 +0800

    Implement SceneDelegate for multi-scene support and update AppDelegate for scene lifecycle management
    - Introduce SceneDelegate to manage window scenes and root view controller setup
    - Update AppDelegate to handle scene configuration and lifecycle events
    - Modify Info.plist to support multiple scenes configuration

--- ProgressHUD/app/AppDelegate.swift
+++ ProgressHUD/app/AppDelegate.swift
@@ -18,14 +18,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
 
-		window = UIWindow(frame: UIScreen.main.bounds)
-
-		let viewController = ViewController(nibName: "ViewController", bundle: nil)
-		let navController = NavigationController(rootViewController: viewController)
-
-		window?.rootViewController = navController
-		window?.makeKeyAndVisible()
-
 		return true
+	}
+
+	// MARK: - UISceneSession Lifecycle
+
+	func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+		return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+	}
+
+	func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
 	}
 }
---
+++ ProgressHUD/app/SceneDelegate.swift
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2025 Related Code - https://relatedcode.com
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+
+		window = UIWindow(windowScene: windowScene)
+
+		let viewController = ViewController(nibName: "ViewController", bundle: nil)
+		let navController = NavigationController(rootViewController: viewController)
+
+		window?.rootViewController = navController
+		window?.makeKeyAndVisible()
+	}
+}
